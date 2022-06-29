edcf2aca510656a4deb2cc8bba2e2f23482c0f92
CMALuca <72840604+CMALuca@users.noreply.github.com>
2022-06-28 19:45:59 -0700

    completed bird-watcher

---
+++ bird-watcher/bird-watcher.js
@@ -0,0 +1,50 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  let total = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    total += birdsPerDay[i];
+  }
+  return total;
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  let total = 0;
+  const indexOfLastDay = (week*7)-1;
+  const indexOfFirstDay = indexOfLastDay-6;
+  for (let i = indexOfFirstDay; i <= indexOfLastDay; i++) {
+      total += birdsPerDay[i];
+    }
+    return total;
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i=0; i < birdsPerDay.length; i += 2) {
+    birdsPerDay[i] = birdsPerDay[i] += 1;
+  }
+  return birdsPerDay;
+}
